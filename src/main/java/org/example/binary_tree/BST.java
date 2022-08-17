0bca71386e3ba5d3a55cf90cd0c26713680e515b
Helmy <mohamed0132525715@gmail.com>
2022-08-17 12:09:24 +0200

    feat: add remove function to BST

--- src/main/java/org/example/binary_tree/BST.java
+++ src/main/java/org/example/binary_tree/BST.java
@@ -90,6 +90,108 @@ public class BST {
     }
 
     /**
+     * Removes the first occurrence of the specified value from the tree.
+     *
+     * @param value The value to remove
+     * @return True if the value was removed, false otherwise<
+     */
+    boolean remove(int value) {
+        var temp = remove(root, value);
+        if (temp == null)
+            return false;
+        else {
+            size--;
+            root = temp;
+            return true;
+        }
+    }
+
+    // Function to delete a node from a BST
+    private Node remove(Node root, int key) {
+        Node parent = null;
+
+        Node curr = root;
+
+        // search key in the BST and set its parent pointer
+        while (curr != null && curr.value != key) {
+            parent = curr;
+            if (key < curr.value) {
+                curr = curr.left;
+            } else {
+                curr = curr.right;
+            }
+        }
+
+        // return if the key is not found in the tree
+        if (curr == null) {
+            return root;
+        }
+
+        // Case 1: node to be deleted has no children, i.e., it is a leaf node
+        if (curr.left == null && curr.right == null) {
+            // if the node to be deleted is not a root node, then set its
+            // parent left/right child to null
+            if (curr != root) {
+                if (parent.left == curr) {
+                    parent.left = null;
+                } else {
+                    parent.right = null;
+                }
+            }
+            // if the tree has only a root node, set it to null
+            else {
+                root = null;
+            }
+        }
+
+        // Case 2: node to be deleted has two children
+        else if (curr.left != null && curr.right != null) {
+            // find its inorder successor node
+            Node successor = getMinimumKey(curr.right);
+
+            // store successor value
+            int val = successor.value;
+
+            // recursively delete the successor. Note that the successor
+            // will have at most one child (right child)
+            remove(root, successor.value);
+
+            // copy value of the successor to the current node
+            curr.value = val;
+        }
+
+        // Case 3: node to be deleted has only one child
+        else {
+            // choose a child node
+            Node child = (curr.left != null) ? curr.left : curr.right;
+
+            // if the node to be deleted is not a root node, set its parent
+            // to its child
+            if (curr != root) {
+                if (curr == parent.left) {
+                    parent.left = child;
+                } else {
+                    parent.right = child;
+                }
+            }
+
+            // if the node to be deleted is a root node, then set the root to the child
+            else {
+                root = child;
+            }
+        }
+
+        return root;
+    }
+
+    public static Node getMinimumKey(Node curr) {
+        while (curr.left != null) {
+            curr = curr.left;
+        }
+        return curr;
+    }
+
+    /**
      * Performs the provided action on each binary tree value in pre-order traversal order
      *
      * @param action The action to perform
